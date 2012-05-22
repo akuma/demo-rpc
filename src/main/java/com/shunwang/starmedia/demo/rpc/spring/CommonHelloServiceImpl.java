f1445370fd804c0d856d43a6760a85204a1a8058
Akuma Huang <ihuangwj@gmail.com>
2012-05-22 11:29:19 +0800

    添加通用的 HelloService 实现类，删除 httpinvoker、spring rmi 中的实现类

--- src/main/java/com/shunwang/starmedia/demo/rpc/spring/CommonHelloServiceImpl.java
+++ src/main/java/com/shunwang/starmedia/demo/rpc/spring/CommonHelloServiceImpl.java
@@ -1,22 +1,22 @@
 /* 
- * @(#)SpringRmiHelloServiceImpl.java    Created on 2012-5-18
+ * @(#)CommonHelloServiceImpl.java    Created on 2012-5-18
  * Copyright (c) 2005-2012 Shunwang. All rights reserved.
  * $Id$
  */
-package com.shunwang.starmedia.demo.rpc.spring.rmi.server;
+package com.shunwang.starmedia.demo.rpc.spring;
 
 import org.springframework.stereotype.Component;
 
 import com.shunwang.starmedia.demo.rpc.common.HelloService;
 
 /**
- * Spring RMI 的服务提供类。
+ * 通用的 HelloService 的服务提供类。
  * 
  * @author wj.huang
  * @version $Revision: 1.0 $, $Date: 2012-5-18 下午5:06:19 $
  */
 @Component
-public class SpringRmiHelloServiceImpl implements HelloService {
+public class CommonHelloServiceImpl implements HelloService {
 
     @Override
     public String sayHello(String name) {
